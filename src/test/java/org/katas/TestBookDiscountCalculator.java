ebb21420997adcb951742cdac4037fcca3b5353f
jsubra10 <jsubra10@ford.com>
2025-04-24 18:46:31 +0530

    Added first tests cases to validate the discount of single book

---
+++ src/test/java/org/katas/TestBookDiscountCalculator.java
@@ -0,0 +1,23 @@
+package org.katas;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestBookDiscountCalculator {
+    @Test
+    public void testSingleBookNoDiscount() {
+        Map<BookTitle, Integer> bookMap = new HashMap<>();
+        bookMap.put(BookTitle.CLEAN_CODE, 1);
+
+        BookBasket basket = new BookBasket(bookMap);
+
+        PriceCalculator calculator = new PriceCalculator();
+        double price = calculator.calculatePrice(basket);
+
+        assertEquals(50.0, price);
+    }
+}
