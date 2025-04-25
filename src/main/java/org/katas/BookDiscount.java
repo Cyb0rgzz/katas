3a91ab51f6e1615c2593ace9336558a1cf4634e0
jsubra10 <jsubra10@ford.com>
2025-04-25 22:39:36 +0530

    updated discount for 4 and 5 books discounts

--- src/main/java/org/katas/BookDiscount.java
+++ src/main/java/org/katas/BookDiscount.java
@@ -3,7 +3,9 @@ package org.katas;
 public enum BookDiscount {
     NONE(0.0),
     TWO_BOOKS(0.05),
-    THREE_BOOKS(0.10);
+    THREE_BOOKS(0.10),
+    FOUR_BOOKS(0.20),
+    FIVE_BOOKS(0.25);
 
     private final double discountRate;
 
@@ -19,6 +21,8 @@ public enum BookDiscount {
         return switch (uniqueBooks) {
             case 2 -> TWO_BOOKS;
             case 3 -> THREE_BOOKS;
+            case 4 -> FOUR_BOOKS;
+            case 5 -> FIVE_BOOKS;
             default -> NONE;
         };
     }
