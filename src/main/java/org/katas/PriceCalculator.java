2d4518366541fbc0ddafe425425d768388512c04
jsubra10 <jsubra10@ford.com>
2025-04-25 22:27:30 +0530

    updated core class to handle the discounts

--- src/main/java/org/katas/PriceCalculator.java
+++ src/main/java/org/katas/PriceCalculator.java
@@ -1,10 +1,21 @@
 package org.katas;
+import java.util.Map;
 
 public class PriceCalculator {
 
     private static final double BOOK_PRICE = 50.0;
+    private final DiscountStrategyFactory discountStrategyFactory;
+
+    public PriceCalculator(DiscountStrategyFactory discountStrategyFactory) {
+        this.discountStrategyFactory = discountStrategyFactory;
+    }
 
     public double calculatePrice(BookBasket basket) {
-        return basket.getBookMap().values().stream().mapToInt(Integer::intValue).sum() * BOOK_PRICE;
+        Map<BookTitle, Integer> bookMap = basket.getBookMap();
+        int totalBooks = bookMap.values().stream().mapToInt(Integer::intValue).sum();
+        double basePrice = totalBooks * BOOK_PRICE;
+
+        DiscountStrategy strategy = discountStrategyFactory.createStrategy(bookMap);
+        return strategy.applyDiscount(basePrice);
     }
 }
